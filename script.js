54d7e685ff442c80eb0a492c4deb8487f85199ef
Yvan Ngassa <yvanngassa@gmail.com>
2016-10-06 21:16:56 +0300

    nkoo
    nkoo

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#bbb').hover(function(){
+        $(this).animate({color:'red'},1000);
+        });
+    });
+
+
+
+$(document).ready(function(){
+    $('.click').click(function(){
+  		$(this).toggleClass('clicked');
+			});
+    });
+
+
+
+function showDiv() {
+   document.getElementById('welcomeDiv').style.display = "block";
+};
+
+
+// Instantiate the Bootstrap carousel
